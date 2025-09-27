2db9a4583bca1b598c529659cfad079e9b3ccd39
Nadiaiscute2016 <ojinadiaoa@gmail.com>
2025-09-27 19:34:51 +0200

    Commit

---
+++ Lesson 1/main.py
@@ -0,0 +1,9 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+#Print(4)
+#Next line after print
+print("Hello Python \n")
+#Pass multiple arguments for printing
+print("Hello, 5")
+#End argument of print
+print("welcome to" ,end="*")
---
+++ Lesson 1/main2.py
@@ -0,0 +1,10 @@
+# Creating different variables
+x = 5
+y = "Nadia"
+print(x)
+print(y)
+Nadia = "Nadia"
+print("Nadia")
+# User Input
+name = input("enter your name:")
+print("\nHello", name, ("\nHi, I'm Nadia"))
