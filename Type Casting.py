e36d7e94bc86d46a75eeb0dcc8c6cdd48e91525e
Nadiaiscute2016 <ojinadiaoa@gmail.com>
2025-10-03 19:04:00 +0200

    commit1

---
+++ Type Casting.py
@@ -0,0 +1,21 @@
+#Assign Different Variables
+name= "Nadia"
+age= "9"
+is_student =True
+weight=38.5
+#Printing Different Variables and Their Data Type
+print("Name: =,name")
+print("Data Type of Name is",(name)) 
+print("Age: =,age")
+print("Data Type of Age is",(age))
+print("is_student: =,is_student")
+print("Data Type of is_student is",)
+type=is_student
+print("Weight: =,weight")
+print("Data Type of Weight is", type(weight)) 
+print("/n After Type Casting...")
+age=str(age)
+print(age)
+print("Data Type of Age is",type(age))
+weight=int(weight)
+print("Data Type of Weight is", type(weight)) 
