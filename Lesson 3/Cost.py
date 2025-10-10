0c9536365c1c32f85897c8467ff3b355ed1a35d2
Nadiaiscute2016 <ojinadiaoa@gmail.com>
2025-10-10 18:55:20 +0200

    commit1

---
+++ Lesson 3/Cost.py
@@ -0,0 +1,8 @@
+costprice=int(input("PLEASE ENTER THE CP"))
+sellingprice=int(input("PLEASE ENTER THE SP"))
+if (sellingprice >costprice):
+ print("profit")
+ pt=sellingprice-costprice
+ print(pt)
+else: 
+ print("No profit")
---
+++ Lesson 3/quiz.py
@@ -0,0 +1,9 @@
+#quiz
+answer=input("What can't you eat for breakfast?")
+if answer == "lunch or dinner":
+ print("Correct!")
+else: print(f"The answer is lunch or dinner, not {answer!r}")
+answer=input("What can you catch, but not throw?")
+if answer == "a cold":
+ print("Correct!")
+else: print(f"The answer is a cold, not {answer!r}")
